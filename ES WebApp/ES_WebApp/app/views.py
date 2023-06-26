1ca64f653e7d254c335574865b1fa6787c9b8b71
kahbodaeini <kahbodaeini@gmail.com>
2023-06-26 21:48:39 +0330

    Back end Complete

--- ES WebApp/ES_WebApp/app/views.py
+++ ES WebApp/ES_WebApp/app/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import os
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
+TEMPLATE_DIRS = (
+    os.path.join(DIRNAME, 'site-templates/'),
+)
 
 
 def index(request):
 
 
-    file = open('./var/tmp/coleman/API.txt', 'r')
-    line = file.getline()
+    file = open('/var/tmp/coleman/API.txt', 'r')
+    line = file.readline()
     WLP = int(line.split()[1])
 
     template = loader.get_template("app/index.html")
     context = {
         "WLP": WLP,
     }
+    return render(request, 'app/index.html', context=context)
     return HttpResponse(template.render(context, request))
